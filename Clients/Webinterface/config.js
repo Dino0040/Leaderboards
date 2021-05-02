d8fdca9c8e048136745ad8a9a4d06d7e82d6b53e
Dino0040 <8014061+Dino0040@users.noreply.github.com>
2021-05-02 21:37:12 +0000

    Create separate config file for webinterface client

---
+++ Clients/Webinterface/config.js
@@ -0,0 +1,2 @@
+var PAGETITLE = 'Exploit Avoid Leaderboards';
+var OAUTHLINK = 'https://itch.io/user/oauth?client_id=ebeb9db39a8145ccb3defe5466838533&scope=profile%3Ame&response_type=token&redirect_uri=https%3A%2F%2Fexploitavoid.com%2Fleaderboards%2F';
