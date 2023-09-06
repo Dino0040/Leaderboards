60e651e4222792af874068a7b5e4280c8f21ba59
Dino0040 <8014061+Dino0040@users.noreply.github.com>
2023-09-06 17:08:43 +0200

    Update config.js with default API endpoint

--- Clients/Webinterface/config.js
+++ Clients/Webinterface/config.js
@@ -1,2 +1,3 @@
 var PAGETITLE = 'Exploit Avoid Leaderboards';
-var OAUTHLINK = 'https://itch.io/user/oauth?client_id=ebeb9db39a8145ccb3defe5466838533&scope=profile%3Ame&response_type=token&redirect_uri=https%3A%2F%2Fexploitavoid.com%2Fleaderboards%2F';
+var APIENDPOINT = '/leaderboards/dev/api/';
+var OAUTHLINK = 'https://itch.io/user/oauth?client_id=ebeb9db39a8145ccb3defe5466838533&scope=profile%3Ame&response_type=token&redirect_uri=https%3A%2F%2Fexploitavoid.com%2Fleaderboards%2Fv1%2F';
